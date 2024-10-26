a05313e6a6ed4725d09c282b4a800e97eab6c2b5
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-26 16:13:00 +0200

    Ej. 2.1.1 0.01

--- src/ej21_1.py
+++ src/ej21_1.py
@@ -1,1 +1,9 @@
-print("hola")
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
