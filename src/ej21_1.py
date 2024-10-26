57a11d9db0a454302826ce9fed77c5ef35969299
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-26 16:19:56 +0200

    Ej. 2.1.1 0.02

--- src/ej21_1.py
+++ src/ej21_1.py
@@ -1,7 +1,17 @@
 # Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 
 
+def pedir_edad():
+    edad = int(input("Introduce su edad: "))
+
+    while edad < 0 or edad > 140:
+        edad = int(input("**ERROR** Edad invalida itroduzca una edad entre 0 y 140: "))
+    return edad
+
+
 def main():
+    edad = pedir_edad()
+    print(edad)
 
     return 0
 
