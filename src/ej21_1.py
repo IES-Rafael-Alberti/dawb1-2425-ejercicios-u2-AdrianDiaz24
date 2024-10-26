caf6ae67cfe14c49a6a8757e27ac921a0479c23d
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-26 18:54:35 +0200

    ej. 2.1.1 1.00

--- src/ej21_1.py
+++ src/ej21_1.py
@@ -5,15 +5,15 @@ def pedir_edad():
     edad = int(input("Introduce su edad: "))
 
     while edad < 0 or edad > 140:
-        edad = int(input("**ERROR** Edad invalida itroduzca una edad entre 0 y 140: "))
+        edad = int(input("**ERROR** Edad invalida introduzca una edad entre 0 y 140: "))
     return edad
 
 
 def comprbar_edad(edad):
     if edad <18:
-        print("Usted no es mayor de edad.")
+        print("- Usted no es mayor de edad.")
     else:
-        print("Usted es mayor de edad.")
+        print("- Usted es mayor de edad.")
     return 0
 
 
