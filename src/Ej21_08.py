3ec6c507de75f838a4a2d69687eb9e26cc903205
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-27 12:00:59 +0100

    Ej. 2.1.8 0.01

--- src/Ej21_08.py
+++ src/Ej21_08.py
@@ -4,4 +4,35 @@
 # traduciéndose en mejores beneficios. Los puntos que pueden conseguir los empleados pueden ser 0.0, 0.4, 0.6 o más, pero no valores intermedios entre las cifras mencionadas. 
 # A continuación se muestra una tabla con los niveles correspondientes a cada puntuación. La cantidad de dinero conseguida en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.
 
+def pedir_puntuacion():
+    puntuacion = float(input("Introduzaca su puntuacion (0.0, 0.4, 0.6 o mas): "))
+    while puntuacion == 0.0 or puntuacion == 0.4 or puntuacion >= 0.6:
+        return puntuacion
+    print("**ERROR** Valor invalido")
 
+
+def comprobar_puntuacion(puntuacion):
+    if puntuacion == None:
+        puntuacion = pedir_puntuacion()
+    return puntuacion
+
+
+def comprobar_rendimiento(puntuacion):
+    if puntuacion == 0.0:
+        rendimiento = "Inaceptable"
+    elif puntuacion == 0.4:
+        rendimiento = "Aceptable"
+    else:
+        rendimiento = "Meritorio" 
+    return rendimiento
+
+
+def main():
+    puntuacion = pedir_puntuacion()
+    puntuacion = comprobar_puntuacion(puntuacion)
+    rendimiento = comprobar_rendimiento(puntuacion)
+    print (rendimiento)
+    return 0
+
+if __name__ == "__main__":
+    main()
