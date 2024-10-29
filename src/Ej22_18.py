f5bd301bb7c8478a5daaa7959a91f0276640379c
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-10-29 20:39:46 +0100

    Ej. 2.2.18 0.01

---
+++ src/Ej22_18.py
@@ -0,0 +1,33 @@
+# Solicitar al usuario que ingrese números enteros positivos y, por cada uno, imprimir la suma de los dígitos que lo componen. 
+# La condición de corte es que se ingrese el número -1. Al finalizar, mostrar cuántos de los números ingresados por el usuario 
+# fueron números pares.
+
+
+def pedir_num():
+    num = int(input("Introduce un Nº: "))
+    while num < -1:
+        num = int(input("**ERROR** Introduce un Nº: "))
+    return num
+
+
+def sumar_digitos(num) -> str:
+    a = len(str(num))
+    num = str(num)
+    suma = 0
+    for i in range(0, a, +1):
+        b = num[i]
+        suma = suma + int(b)
+    return suma
+
+
+def main():
+    print("Introduce el -1 para cerrar el programa.")
+    num = pedir_num()
+    while num != -1:
+        suma = sumar_digitos(num)
+        print(f"La suma de los digitos del Nº dado es {suma}")
+        num = pedir_num()
+    return 0
+
+if __name__ == "__main__":
+    main()
