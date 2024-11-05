349703bff7f819295b39a5453e4e035d00adacaa
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-05 15:45:40 +0100

    Ej. 2.2.19 1.02

--- src/Bucles/Ej22_19.py
+++ src/Bucles/Ej22_19.py
@@ -3,6 +3,7 @@
 # El menú se debe volver a mostrar luego de ejecutada cada opción, permitiendo volver a elegir. Si elige las opciones 1 ó 2 se 
 # imprimirá un texto. Si elige la opción 3, se interrumpirá la impresión del menú y el programa finalizará.
 
+import time
 
 def mostrar_menu():
     num = int(input("MENU\n1. comenzar programa\n2. imprimir listado\n3. finalizar programa\n -> "))
@@ -12,12 +13,45 @@ def mostrar_menu():
 
 
 def programa_1():
-    print("Elegiste la opcion 1\nEjecutando el programa.\nEjecutando el programa..\nEjecutando el programa...\n**ERROR AL EJECUTAR EL PROGRAMA**\nVolviendo al menu. ")
+    print("Elegiste la opcion 1\n")
+    time.sleep(1)
+    print("Ejecutando el programa.")
+    time.sleep(1)
+    print("Ejecutando el programa..")
+    time.sleep(1)
+    print("Ejecutando el programa...\n")
+    time.sleep(1)
+    print("**ERROR AL EJECUTAR EL PROGRAMA**\n")
+    time.sleep(1)
+    print("Volviendo al menu.\n")
+    time.sleep(1)
     return 0
 
 
 def listado():
-    print("Elegiste la opcion 2\nImprimiendo el listado.\nImprimiendo el listado..\nImprimiendo el listado...\n**ERROR AL IMPRIMIR EL LISTADO**\nVolviendo al menu. ")
+    print("Elegiste la opcion 2\n")
+    time.sleep(1)
+    print("Imprimiendo el listado.")
+    time.sleep(1)
+    print("Imprimiendo el listado..")
+    time.sleep(1)
+    print("Imprimiendo el listado...\n")
+    time.sleep(1)
+    print("**ERROR AL IMPRIMIR EL LISTADO**\n")
+    time.sleep(1)
+    print("Volviendo al menu.\n")
+    time.sleep(1)
+    return 0
+
+
+def salir():
+    print("\nSaliendo.")
+    time.sleep(1)
+    print("Saliendo..")
+    time.sleep(1)
+    print("Saliendo...\n")
+    time.sleep(1)
+    print("Saliste")
     return 0
 
 
@@ -31,8 +65,8 @@ def main():
         elif num == 2:
             listado()
             num = mostrar_menu()
-        else:
-            print("salinedo...")
+        elif  num == 3:
+            salir()
     return 0
 if __name__ == "__main__":
     main()
