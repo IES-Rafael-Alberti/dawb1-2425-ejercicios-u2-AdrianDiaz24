0adcb90a0818fbe67f0d72fa1da06d11cc09a8f2
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-05 20:48:23 +0100

    Ej.2.2.22 0.03

--- src/Bucles/Ej22_22.py
+++ src/Bucles/Ej22_22.py
@@ -12,8 +12,26 @@ def pedir_num():
         return num
 
 
+def comprobar_num(num):
+    if num%2 == 0:
+        num_c = 1
+    else:
+        num_c = 0
+    return num_c
+
+
 def main():
-    num = pedir_num()
+    num_p = 0
+    num_i = 0
+    while True:
+        num = pedir_num()
+        if num == 0:
+            print(f"\nTotal de Nº pares: {num_p}\nTotal de Nº impares: {num_i}\n")
+            break
+        else:
+            long = len(num)
+            for i in range(0, long, +1):
+                num_c = comprobar_num(num[i])
     return 0
 
 if __name__ == "__main__":
