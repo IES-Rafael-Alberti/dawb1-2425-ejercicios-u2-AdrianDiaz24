02500c6ea8648c923b4634f4aee4aad1b716dc05
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-07 14:41:22 +0100

    Ej. 2.2.23 0.01

--- src/Bucles/Ej22_23.py
+++ src/Bucles/Ej22_23.py
@@ -6,8 +6,36 @@
 # componen en esa línea). Finalmente, informar cuántas líneas completas se ingresaron.
 
 
+def comprobar_cadena(cadena:str):
+    contador = 0
+    i = 0
+    if cadena == "*":
+        salir()
+    else:
+        long = len(cadena)
+        while contador < 10 and i != long-1:
+            for i in range(0, long, +1):
+                comprobacion = cadena[i].isdigit()
+                if comprobacion == False:
+                    continue
+                elif comprobacion == True:
+                    contador += 1
+                elif i == long:
+                    return contador 
+    return contador
+
+
+def salir():
+
+    return 0
+
+
 def main():
-    
+
+    while True:
+        cadena = str(input("Introduce el titulo de un libro: "))
+        i = comprobar_cadena(cadena)
+        print(i)
     return 0
 
 if __name__ == "__main__":
