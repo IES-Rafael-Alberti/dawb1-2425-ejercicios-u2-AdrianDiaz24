a5b3975ec0e09d26fcf33805f5b9c086a81600c1
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-07 17:53:07 +0100

    Ej. 3.3.1 0.00

---
+++ src/Excepciones/Ej23_01.py
@@ -0,0 +1,10 @@
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha 
+# cumplido (desde 1 hasta su edad).
+
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
