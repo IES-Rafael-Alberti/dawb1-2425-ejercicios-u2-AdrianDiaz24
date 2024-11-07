9eaebaa5c495fa2c0cd98f3e9e6db3276d4e756c
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-07 21:18:53 +0100

    Ej. 2.4 0.01

---
+++ src/Depuracion/Ej24_01.py
@@ -0,0 +1,19 @@
+# Ahora que ya sabemos como funciona el algoritmo de burbuja, pasemos a la práctica. 
+# Implementación en Python y utiliza el debugger para asegurarte que funciona adecuadamente y 
+# entiendes su funcionamiento.
+
+
+def main():
+    a = [8, 3, 1, 19, 14]
+    b = len(a)
+    print(b)
+    return
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
