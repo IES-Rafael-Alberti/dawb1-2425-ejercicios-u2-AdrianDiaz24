c3654ece8d1879ea1e9e23fe6023089026bc6cb9
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-07 19:39:21 +0100

    Ej. 3.3.1 1.00

--- src/Excepciones/Ej23_01.py
+++ src/Excepciones/Ej23_01.py
@@ -2,9 +2,34 @@
 # cumplido (desde 1 hasta su edad).
 
 
-def main():
+def pedir_edad():
+    while True:
+        try:
+            edad = str(input("Introduce tu edad: "))
+            while int(edad) < 0 or int(edad) > 140:
+                if int(edad) < 0:
+                    print("**ERROR** Tu edad no puede ser negativa.")
+                    edad = str(input("Introduce tu edad: "))
+                elif int(edad) > 140:
+                    print("**ERROR** No te flipes que no puedes terer mas de 140 años.")
+                    edad = str(input("Introduce tu edad: "))
+            return edad
+        except ValueError:
+            if edad.find(".") == True:
+                print("**ERROR** La edad no puede ser un numero con decimales")
+            elif edad.isalpha() == True:
+                print("**ERROR** Introduce un numero no una letra.")
 
-    return 0
+
+
+def main():
+    try:
+        edad = pedir_edad()
+        for i in range(1, int(edad)+1, +1):
+            print(i)
+    except:
+
+        return 0
 
 if __name__ == "__main__":
     main()
