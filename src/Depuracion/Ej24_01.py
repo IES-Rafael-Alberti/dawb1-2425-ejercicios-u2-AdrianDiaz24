55c75f69130098155c25d5e990ac44ff5bb8ea89
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2024-11-08 23:52:17 +0100

    Ej. 2.4.1 1.02

--- src/Depuracion/Ej24_01.py
+++ src/Depuracion/Ej24_01.py
@@ -3,14 +3,28 @@
 # entiendes su funcionamiento.
 
 
-def main():
-    a = [8, 3, 1, 19, 14, 50, 2, 71, 4, 22, 39]
+def ordenar_lista(lista):
+    """_summary_
+
+    Args:
+        lista (array): La lista escrita por el usuario con el orden original
+
+    Returns:
+        array: La lista ordenada de menor a mayor
+    """
     i = 0
-    b = len(a)
+    b = len(lista)
     for i in range(0, b-1-i, +1):
         for j in range(0, b-1-i, +1):
-            if a[j] > a[j+1]:
-                a[j], a[j+1] = a[j+1], a[j]
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+    return lista
+
+
+def main():
+    lista = [8, 3, 1, 19, 14, 50, 2, 71, 4, 22, 39]
+    lista = ordenar_lista(lista)
+    print(f"La lista ordenada es {lista}")
     return
 
 if __name__ == "__main__":
